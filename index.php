ad463ce19710a23fa89dd14b4617313cb42af209
snikliwp <wilk0146@algonquinlive.com>
2012-01-23 11:13:12 -0500

    initial load

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Contact Form</title>
+	<link href="css/general.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+
+
+
+
+</body>
+</html>
